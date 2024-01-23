9458247840c169d4a34ae156884c9058bbf8f31a
Robin Carrupt <robincarrupt@gmail.com>
2024-01-23 20:28:52 +0100

    PID for hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -662,9 +662,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  23.36
+    #define DEFAULT_Ki   1.76
+    #define DEFAULT_Kd  77.39
   #endif
 #endif
 
