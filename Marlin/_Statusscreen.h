a035d60a072d96f24c9c9bef5870bbf48c55266b
Robin Carrupt <robincarrupt@gmail.com>
2024-02-01 12:59:49 +0100

    New bootscreen

--- Marlin/_Statusscreen.h
+++ Marlin/_Statusscreen.h
@@ -21,37 +21,35 @@
  */
 #pragma once
 
-#define CONFIG_EXAMPLES_DIR "Creality/CR-10/BigTreeTech SKR Mini E3 2.0"
+#define CONFIG_EXAMPLES_DIR "Creality/CR-10/CrealityV1"
 
-/**
- * Custom Status Screen bitmap
- *
- * Place this file in the root with your configuration files
- * and enable CUSTOM_STATUS_SCREEN_IMAGE in Configuration.h.
- *
- * Use the Marlin Bitmap Converter to make your own:
- * https://marlinfw.org/tools/u8glib/converter.html
- */
-
-//
-// Status Screen Logo bitmap
-//
-#define STATUS_LOGO_X            8
-#define STATUS_LOGO_Y            5
-#define STATUS_LOGO_WIDTH       22
+#define STATUS_LOGO_WIDTH  24
 
 const unsigned char status_logo_bmp[] PROGMEM = {
-  B00111110,B00001111,B11110000,
-  B11000001,B10000110,B00011000,
-  B11000001,B10000110,B00001100,
-  B00000001,B10000110,B00000100,
-  B00000011,B00000110,B00000100,
-  B00011111,B00000110,B00000100,
-  B00000011,B00000110,B00000100,
-  B00000001,B10000110,B00000100,
-  B11000001,B10000110,B00001100,
-  B11000001,B10000110,B00011000,
-  B00111110,B00001111,B11110000
+  0x00,0x0C,0x00, // ............##..........
+  0x00,0x7F,0x00, // .........#######........
+  0x01,0xBF,0xA0, // .......##.#######.#.....
+  0x07,0xCF,0xB0, // .....#####..#####.##....
+  0x0F,0xF7,0xB8, // ....########.####.###...
+  0x1F,0xFB,0xB8, // ...##########.###.###...
+  0x3C,0x00,0xBC, // ..####..........#.####..
+  0x23,0x00,0x3E, // ..#...##..........#####.
+  0x1E,0x00,0x3E, // ...####...........#####.
+  0x7C,0x00,0x3D, // .#####............####.#
+  0x7C,0x00,0x1B, // .#####.............##.##
+  0x78,0x00,0x1B, // .####..............##.##
+  0xF4,0x00,0x17, // ####.#.............#.###
+  0xEC,0x00,0x0F, // ###.##..............####
+  0x6C,0x00,0x0F, // .##.##..............####
+  0x5C,0x00,0x1F, // .#.###.............#####
+  0x3C,0x00,0x1E, // ..####.............####.
+  0x3D,0x00,0x30, // ..####.#..........##....
+  0x1D,0x80,0x0C, // ...###.##...........##..
+  0x1D,0xCF,0xF8, // ...###.###..#########...
+  0x0E,0xF3,0xF8, // ....###.####..#######...
+  0x02,0xFD,0xE0, // ......#.######.####.....
+  0x02,0xFE,0xC0, // ......#.#######.##......
+  0x00,0x7E,0x00  // .........######.........
 };
 
 //
